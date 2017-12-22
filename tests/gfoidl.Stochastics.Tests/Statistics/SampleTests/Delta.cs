304f94d59a86f330c3c677f85f26319e0d78482f
Günther Foidl <gue@korporal.at>
2017-12-22 23:11:42 +0100

    Instead of local functions explicit internal functions -> cross testing is possible
    By a test the result of the simd version and the parallelized simd version can be compared -- cross testing. Otherwise it might be difficult to test the functionality accurately.

--- tests/gfoidl.Stochastics.Tests/Statistics/SampleTests/Delta.cs
+++ tests/gfoidl.Stochastics.Tests/Statistics/SampleTests/Delta.cs
@@ -1,4 +1,5 @@
-﻿using gfoidl.Stochastics.Statistics;
+﻿using System;
+using gfoidl.Stochastics.Statistics;
 using NUnit.Framework;
 
 namespace gfoidl.Stochastics.Tests.Statistics.SampleTests
@@ -25,6 +26,23 @@ namespace gfoidl.Stochastics.Tests.Statistics.SampleTests
             double actual = sut.Delta;
 
             Assert.AreEqual(0, actual, 1e-10);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Simd_and_ParallelizedSimd_produce_same_result()
+        {
+            var values = new double[_size];
+            var rnd    = new Random();
+
+            for (int i = 0; i < values.Length; ++i)
+                values[i] = rnd.NextDouble();
+
+            var sut = new Sample(values);
+
+            double actual1 = sut.CalculateDeltaSimd();
+            double actual2 = sut.CalculateDeltaParallelizedSimd();
+
+            Assert.AreEqual(actual1, actual2, 1e-10);
         }
     }
 }
