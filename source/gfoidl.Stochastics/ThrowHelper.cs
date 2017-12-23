1735482398814c0b39e153b7b4be5607079d7598
Günther Foidl <gue@korporal.at>
2017-12-23 11:26:33 +0100

    Nesting of Sample.*.cs in csproj

--- source/gfoidl.Stochastics/ThrowHelper.cs
+++ source/gfoidl.Stochastics/ThrowHelper.cs
@@ -3,11 +3,11 @@ using System.Runtime.CompilerServices;
 
 namespace gfoidl.Stochastics
 {
-	internal static class ThrowHelper
-	{
-		internal static void ThrowArgumentNull(string argName) => throw CreateArgumentNull(argName);
+    internal static class ThrowHelper
+    {
+        internal static void ThrowArgumentNull(string argName) => throw CreateArgumentNull(argName);
 
-		[MethodImpl(MethodImplOptions.NoInlining)]
-		private static Exception CreateArgumentNull(string argName) => new ArgumentNullException(argName);
-	}
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static Exception CreateArgumentNull(string argName) => new ArgumentNullException(argName);
+    }
 }
