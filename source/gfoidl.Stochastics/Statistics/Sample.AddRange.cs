2094946ee7382086422e676c6081aed90de1b82c
Günther Foidl <gue@korporal.at>
2018-01-19 15:18:12 +0100

    AddRange added to Sample

---
+++ source/gfoidl.Stochastics/Statistics/Sample.AddRange.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using gfoidl.Stochastics.Builders;
+
+namespace gfoidl.Stochastics.Statistics
+{
+    partial class Sample
+    {
+        public IEnumerable<double> AddRange(IEnumerable<double> values)
+        {
+            if (values == null) ThrowHelper.ThrowArgumentNull(nameof(values));
+
+            return Core();
+            //-----------------------------------------------------------------
+            IEnumerable<double> Core()
+            {
+                System.Console.WriteLine(values.GetType());
+
+                double min       = double.MaxValue;
+                double max       = double.MinValue;
+                double avg       = 0;
+                int count        = 0;
+                var arrayBuilder = new ArrayBuilder<double>(true);
+
+                foreach (double item in values)
+                {
+                    arrayBuilder.Add(item);
+
+                    if (item < min) min = item;
+                    if (item > max) max = item;
+
+                    avg += item;
+                    count++;
+
+                    yield return item;
+                }
+
+                _values = arrayBuilder.ToArray();
+                _min    = min;
+                _max    = max;
+                _mean   = avg / count;
+            }
+        }
+    }
+}
