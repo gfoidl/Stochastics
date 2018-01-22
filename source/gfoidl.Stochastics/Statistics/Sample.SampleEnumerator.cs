098385d9d871b4e496d21e84b6582f2ef5f2a83e
Günther Foidl <gue@korporal.at>
2018-01-22 15:26:43 +0100

    ArrayEnumerator introduced, and it will be used instead SampleEnumerator
    Plus for next steps...

--- source/gfoidl.Stochastics/Statistics/Sample.SampleEnumerator.cs
+++ source/gfoidl.Stochastics/Statistics/Sample.SampleEnumerator.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using gfoidl.Stochastics.Enumerators;
 
 namespace gfoidl.Stochastics.Statistics
 {
@@ -8,33 +8,8 @@ namespace gfoidl.Stochastics.Statistics
     {
         public double this[int index] => _values[index];
         //---------------------------------------------------------------------
-        public SampleEnumerator GetEnumerator()                 => new SampleEnumerator(_values);
+        public ArrayEnumerator<double> GetEnumerator()          => new ArrayEnumerator<double>(_values);
         IEnumerator<double> IEnumerable<double>.GetEnumerator() => this.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator()                 => this.GetEnumerator();
-        //---------------------------------------------------------------------
-        public struct SampleEnumerator : IEnumerator<double>
-        {
-            private readonly double[] _values;
-            private int               _index;
-            //---------------------------------------------------------------------
-            public SampleEnumerator(double[] values)
-            {
-                _values = values;
-                _index  = -1;
-            }
-            //---------------------------------------------------------------------
-            public double Current      => _values[_index];
-            object IEnumerator.Current => this.Current;
-            //---------------------------------------------------------------------
-            public void Dispose() { }
-            //---------------------------------------------------------------------
-            public bool MoveNext()
-            {
-                _index++;
-                return _index < _values.Length;
-            }
-            //-----------------------------------------------------------------
-            public void Reset() => _index = -1;
-        }
     }
 }
