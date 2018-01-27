3f851e74247323e81e7743096fcc2ea85021e45f
Günther Foidl <gue@korporal.at>
2018-01-27 19:31:16 +0100

    RuntimeHelper added

---
+++ source/gfoidl.Stochastics/RuntimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+
+namespace gfoidl.Stochastics
+{
+    internal static class RuntimeHelper
+    {
+        private static bool? _isRunningOnDotNetCore;
+        //---------------------------------------------------------------------
+        public static bool IsRunningOnDotNetCore()
+        {
+            if (!_isRunningOnDotNetCore.HasValue)
+            {
+                string frameworkName = Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
+                _isRunningOnDotNetCore = frameworkName.Contains("NETCoreApp");
+            }
+
+            return _isRunningOnDotNetCore.Value;
+        }
+    }
+}
