1a2a603bb5c8aa84d5899f190ee4eafc474ae90b
Günther Foidl <gue@korporal.at>
2018-04-19 15:02:26 +0200

    VectorHelper.ReduceMinMax

--- source/gfoidl.Stochastics/VectorHelper.cs
+++ source/gfoidl.Stochastics/VectorHelper.cs
@@ -55,5 +55,50 @@ namespace gfoidl.Stochastics
 #endif
             return Vector.Dot(Vector<double>.One, vector);
         }
+        //---------------------------------------------------------------------
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ReduceMinMax(Vector<double> minVec, Vector<double> maxVec, ref double min, ref double max)
+        {
+#if NETCOREAPP2_1
+            if (Avx.IsSupported && Sse2.IsSupported && 256 / 8 == sizeof(double) * Vector<double>.Count)
+            {
+                min = MinMaxCore(minVec, true);
+                max = MinMaxCore(maxVec, false);
+                return;
+            }
+#endif
+            for (int j = 0; j < Vector<double>.Count; ++j)
+            {
+                if (minVec[j] < min) min = minVec[j];
+                if (maxVec[j] > max) max = maxVec[j];
+            }
+        }
+        //---------------------------------------------------------------------
+#if NETCOREAPP2_1
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static double MinMaxCore(Vector<double> vector, bool doMin)
+        {
+            Vector256<double> vec256 = Unsafe.As<Vector<double>, Vector256<double>>(ref vector);
+            Vector128<double> hi128  = Avx.ExtractVector128(vec256, 1);
+            Vector128<double> lo128  = Avx.ExtractVector128(vec256, 0);
+            Vector128<double> tmp1   = Avx.Permute(hi128, 0b_01);
+            Vector128<double> tmp2   = Avx.Permute(lo128, 0b_01);
+
+            if (doMin)
+            {
+                hi128 = Sse2.Min(hi128, tmp1);
+                lo128 = Sse2.Min(lo128, tmp2);
+                lo128 = Sse2.Min(lo128, hi128);
+            }
+            else
+            {
+                hi128 = Sse2.Max(hi128, tmp1);
+                lo128 = Sse2.Max(lo128, tmp2);
+                lo128 = Sse2.Max(lo128, hi128);
+            }
+
+            return Sse2.ConvertToDouble(lo128);
+        }
+#endif
     }
 }
