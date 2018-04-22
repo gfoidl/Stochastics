752c303e66097e5e759f4cb84a503c194648397f
Günther Foidl <gue@korporal.at>
2018-04-23 00:14:56 +0200

    Revert "BenchmarkSwitcher used instead of self-baken solution"
    This reverts commit bf00c42139cb3757eb9e06e1099e2400987aaf12.

--- perf/gfoidl.Stochastics.Benchmarks/Program.cs
+++ perf/gfoidl.Stochastics.Benchmarks/Program.cs
@@ -1,4 +1,8 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
 
 namespace gfoidl.Stochastics.Benchmarks
 {
@@ -6,8 +10,59 @@ namespace gfoidl.Stochastics.Benchmarks
     {
         static void Main(string[] args)
         {
-            var switcher = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly);
-            switcher.Run(args);
+            if (args.Length != 1)
+            {
+                Console.WriteLine("one and only arg has to be name of benchmark");
+                PrintAvailableBenchmarks();
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine($"{nameof(Vector.IsHardwareAccelerated)}: {Vector.IsHardwareAccelerated}");
+
+            string type          = args[0];
+            IBenchmark benchmark = GetBenchmark(type);
+
+            if (benchmark == null)
+            {
+                Console.WriteLine($"unknown benchmark '{type}'");
+                PrintAvailableBenchmarks();
+                Environment.Exit(2);
+            }
+
+            benchmark.Run();
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("\nEnd.");
+                Console.ReadKey();
+            }
+        }
+        //---------------------------------------------------------------------
+        private static IBenchmark GetBenchmark(string type)
+        {
+            Assembly assembly = typeof(Program).Assembly;
+
+            Type benchmarkType = assembly
+                .DefinedTypes
+                .FirstOrDefault(t => typeof(IBenchmark).IsAssignableFrom(t) && t.Name == type);
+
+            if (benchmarkType == null) return null;
+
+            return Activator.CreateInstance(benchmarkType) as IBenchmark;
+        }
+        //---------------------------------------------------------------------
+        private static void PrintAvailableBenchmarks()
+        {
+            Assembly assembly = typeof(Program).Assembly;
+
+            var types = assembly
+                .DefinedTypes
+                .Where(t => typeof(IBenchmark).IsAssignableFrom(t) && t != typeof(IBenchmark))
+                .OrderBy(t => t.Name);
+
+            Console.WriteLine("\nAvailable benchmarks:");
+            foreach (Type type in types)
+                Console.WriteLine($"\t{type.Name}");
         }
     }
 }
