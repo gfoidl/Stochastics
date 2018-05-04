fcd26d9437900ca69fdf1c659649a840498404fb
Günther Foidl <gue@korporal.at>
2018-05-04 20:54:58 +0200

    Sample.Delta RAW-fix

--- perf/gfoidl.Stochastics.Benchmarks/BenchmarkDotNet.Artifacts/results/CalculateDeltaISPBenchmarks/report-github.md
+++ perf/gfoidl.Stochastics.Benchmarks/BenchmarkDotNet.Artifacts/results/CalculateDeltaISPBenchmarks/report-github.md
@@ -2,15 +2,15 @@
 
 BenchmarkDotNet=v0.10.11, OS=Windows 10 Redstone 3 [1709, Fall Creators Update] (10.0.16299.371)
 Processor=Intel Core i7-7700HQ CPU 2.80GHz (Kaby Lake), ProcessorCount=8
-Frequency=2742187 Hz, Resolution=364.6724 ns, Timer=TSC
+Frequency=2742191 Hz, Resolution=364.6719 ns, Timer=TSC
 .NET Core SDK=2.1.300-preview3-008618
   [Host]     : .NET Core 2.1.0-preview3-26411-06 (Framework 4.6.26411.07), 64bit RyuJIT
   DefaultJob : .NET Core 2.1.0-preview3-26411-06 (Framework 4.6.26411.07), 64bit RyuJIT
 
 
 ```
-| Method |     Mean |     Error |    StdDev | Scaled | ScaledSD |
-|------- |---------:|----------:|----------:|-------:|---------:|
-|   Base | 3.256 us | 0.0390 us | 0.0365 us |   1.00 |     0.00 |
-|    ISP | 1.734 us | 0.0349 us | 0.0489 us |   0.53 |     0.02 |
-|   ISP1 | 1.542 us | 0.0093 us | 0.0082 us |   0.47 |     0.01 |
+| Method |     Mean |     Error |    StdDev | Scaled |
+|------- |---------:|----------:|----------:|-------:|
+|   Base | 3.263 us | 0.0201 us | 0.0168 us |   1.00 |
+|    ISP | 1.549 us | 0.0097 us | 0.0091 us |   0.47 |
+|   ISP1 | 1.440 us | 0.0133 us | 0.0118 us |   0.44 |
--- perf/gfoidl.Stochastics.Benchmarks/CalculateDeltaISPBenchmarks.cs
+++ perf/gfoidl.Stochastics.Benchmarks/CalculateDeltaISPBenchmarks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
@@ -10,7 +11,7 @@ namespace gfoidl.Stochastics.Benchmarks
         public void Run()
         {
             var benchs = new CalculateDeltaISPBenchmarks();
-            benchs.N = 1000;
+            benchs.N = 123;
             benchs.GlobalSetup();
             const int align = -35;
             Console.WriteLine($"{nameof(benchs.Base),align}: {benchs.Base()}");
@@ -40,7 +41,7 @@ namespace gfoidl.Stochastics.Benchmarks
             for (int i = 0; i < this.N; ++i)
             {
                 _values[i] = rnd.NextDouble();
-                _avg          += _values[i];
+                _avg      += _values[i];
             }
 
             _avg /= this.N;
@@ -157,98 +158,6 @@ namespace gfoidl.Stochastics.Benchmarks
                     var avgVec    = new Vector<double>(avg);
                     var deltaVec0 = Vector<double>.Zero;
                     var deltaVec1 = Vector<double>.Zero;
-
-                    // https://github.com/gfoidl/Stochastics/issues/46
-                    int m = n & ~(8 * Vector<double>.Count - 1);
-                    for (; i < m; i += 8 * Vector<double>.Count)
-                    {
-                        Core(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-                        Core(arr, 1 * Vector<double>.Count, avgVec, ref deltaVec1, end);
-                        Core(arr, 2 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-                        Core(arr, 3 * Vector<double>.Count, avgVec, ref deltaVec1, end);
-                        Core(arr, 4 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-                        Core(arr, 5 * Vector<double>.Count, avgVec, ref deltaVec1, end);
-                        Core(arr, 6 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-                        Core(arr, 7 * Vector<double>.Count, avgVec, ref deltaVec1, end);
-
-                        arr += 8 * Vector<double>.Count;
-                    }
-
-                    m = n & ~(4 * Vector<double>.Count - 1);
-                    if (i < m)
-                    {
-                        Core(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-                        Core(arr, 1 * Vector<double>.Count, avgVec, ref deltaVec1, end);
-                        Core(arr, 2 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-                        Core(arr, 3 * Vector<double>.Count, avgVec, ref deltaVec1, end);
-
-                        arr += 4 * Vector<double>.Count;
-                        i   += 4 * Vector<double>.Count;
-                    }
-
-                    m = n & ~(2 * Vector<double>.Count - 1);
-                    if (i < m)
-                    {
-                        Core(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-                        Core(arr, 1 * Vector<double>.Count, avgVec, ref deltaVec1, end);
-
-                        arr += 2 * Vector<double>.Count;
-                        i   += 2 * Vector<double>.Count;
-                    }
-
-                    m = n & ~(1 * Vector<double>.Count - 1);
-                    if (i < m)
-                    {
-                        Core(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, end);
-
-                        arr += 1 * Vector<double>.Count;
-                    }
-
-                    // Reduction -- https://github.com/gfoidl/Stochastics/issues/43
-                    deltaVec0 += deltaVec1;
-                    delta     += deltaVec0.ReduceSum();
-                }
-
-                while (arr < end)
-                {
-                    delta += Math.Abs(*arr - avg);
-                    arr++;
-                }
-            }
-
-            return delta;
-            //-----------------------------------------------------------------
-            void Core(double* arr, int offset, Vector<double> avgVec, ref Vector<double> deltaVec, double* end)
-            {
-#if DEBUG_ASSERT
-                Debug.Assert(arr + offset < end);
-#endif
-                Vector<double> vec = VectorHelper.GetVectorUnaligned(arr + offset);
-                deltaVec += Vector.Abs(vec - avgVec);
-            }
-        }
-        //---------------------------------------------------------------------
-        [Benchmark]
-        public unsafe double ISP1()
-        {
-            int i = 0;
-            int n = this.N;
-
-            double delta = 0;
-            double avg   = this.Mean;
-
-            fixed (double* pArray = _values)
-            {
-                double* arr = pArray + i;
-                n          -= i;
-                i           = 0;
-                double* end = arr + n;
-
-                if (Vector.IsHardwareAccelerated && n >= Vector<double>.Count)
-                {
-                    var avgVec    = new Vector<double>(avg);
-                    var deltaVec0 = Vector<double>.Zero;
-                    var deltaVec1 = Vector<double>.Zero;
                     var deltaVec2 = Vector<double>.Zero;
                     var deltaVec3 = Vector<double>.Zero;
 
@@ -318,8 +227,124 @@ namespace gfoidl.Stochastics.Benchmarks
                 Debug.Assert(arr + offset < end);
 #endif
                 Vector<double> vec = VectorHelper.GetVectorUnaligned(arr + offset);
-                deltaVec          += Vector.Abs(vec - avgVec);
+                deltaVec += Vector.Abs(vec - avgVec);
             }
+        }
+        //---------------------------------------------------------------------
+        [Benchmark]
+        public unsafe double ISP1()
+        {
+            int i = 0;
+            int n = this.N;
+
+            double delta = 0;
+            double avg   = this.Mean;
+
+            fixed (double* pArray = _values)
+            {
+                double* arr = pArray + i;
+                n          -= i;
+                i           = 0;
+                double* end = arr + n;
+
+                if (Vector.IsHardwareAccelerated && n >= Vector<double>.Count)
+                {
+                    var avgVec    = new Vector<double>(avg);
+                    var deltaVec0 = Vector<double>.Zero;
+                    var deltaVec1 = Vector<double>.Zero;
+                    var deltaVec2 = Vector<double>.Zero;
+                    var deltaVec3 = Vector<double>.Zero;
+
+                    // https://github.com/gfoidl/Stochastics/issues/46
+                    int m = n & ~(8 * Vector<double>.Count - 1);
+                    for (; i < m; i += 8 * Vector<double>.Count)
+                    {
+                        ISP1Core4(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, ref deltaVec1, ref deltaVec2, ref deltaVec3, end);
+                        ISP1Core4(arr, 4 * Vector<double>.Count, avgVec, ref deltaVec0, ref deltaVec1, ref deltaVec2, ref deltaVec3, end);
+
+                        arr += 8 * Vector<double>.Count;
+                    }
+
+                    m = n & ~(4 * Vector<double>.Count - 1);
+                    if (i < m)
+                    {
+                        ISP1Core4(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, ref deltaVec1, ref deltaVec2, ref deltaVec3, end);
+
+                        arr += 4 * Vector<double>.Count;
+                        i   += 4 * Vector<double>.Count;
+                    }
+
+                    m = n & ~(2 * Vector<double>.Count - 1);
+                    if (i < m)
+                    {
+                        ISP1Core2(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, ref deltaVec1, end);
+
+                        arr += 2 * Vector<double>.Count;
+                        i   += 2 * Vector<double>.Count;
+                    }
+
+                    m = n & ~(1 * Vector<double>.Count - 1);
+                    if (i < m)
+                    {
+                        ISP1Core(arr, 0 * Vector<double>.Count, avgVec, ref deltaVec0, end);
+
+                        arr += 1 * Vector<double>.Count;
+                    }
+
+                    // Reduction -- https://github.com/gfoidl/Stochastics/issues/43
+                    deltaVec0 += deltaVec1;
+                    deltaVec2 += deltaVec3;
+                    deltaVec0 += deltaVec2;
+                    delta     += deltaVec0.ReduceSum();
+                }
+
+                while (arr < end)
+                {
+                    delta += Math.Abs(*arr - avg);
+                    arr++;
+                }
+            }
+
+            return delta;
+        }
+        //---------------------------------------------------------------------
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe void ISP1Core4(double* arr, int offset, Vector<double> avgVec, ref Vector<double> deltaVec0, ref Vector<double> deltaVec1, ref Vector<double> deltaVec2, ref Vector<double> deltaVec3, double* end)
+        {
+            Vector<double> vec0 = VectorHelper.GetVectorUnaligned(arr + offset + 0 * Vector<double>.Count);
+            Vector<double> vec1 = VectorHelper.GetVectorUnaligned(arr + offset + 1 * Vector<double>.Count);
+            Vector<double> vec2 = VectorHelper.GetVectorUnaligned(arr + offset + 2 * Vector<double>.Count);
+            Vector<double> vec3 = VectorHelper.GetVectorUnaligned(arr + offset + 3 * Vector<double>.Count);
+
+            vec0 -= avgVec;
+            vec1 -= avgVec;
+            vec2 -= avgVec;
+            vec3 -= avgVec;
+
+            deltaVec0 += Vector.Abs(vec0);
+            deltaVec1 += Vector.Abs(vec1);
+            deltaVec2 += Vector.Abs(vec2);
+            deltaVec3 += Vector.Abs(vec3);
+        }
+        //---------------------------------------------------------------------
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe void ISP1Core2(double* arr, int offset, Vector<double> avgVec, ref Vector<double> deltaVec0, ref Vector<double> deltaVec1, double* end)
+        {
+            Vector<double> vec0 = VectorHelper.GetVectorUnaligned(arr + offset + 0 * Vector<double>.Count);
+            Vector<double> vec1 = VectorHelper.GetVectorUnaligned(arr + offset + 1 * Vector<double>.Count);
+
+            vec0 -= avgVec;
+            vec1 -= avgVec;
+
+            deltaVec0 += Vector.Abs(vec0);
+            deltaVec1 += Vector.Abs(vec1);
+        }
+        //---------------------------------------------------------------------
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe void ISP1Core(double* arr, int offset, Vector<double> avgVec, ref Vector<double> deltaVec, double* end)
+        {
+            Vector<double> vec = VectorHelper.GetVectorUnaligned(arr + offset);
+            deltaVec          += Vector.Abs(vec - avgVec);
         }
     }
 }
