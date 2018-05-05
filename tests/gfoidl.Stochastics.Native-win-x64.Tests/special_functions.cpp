6e832377c0e9aea7d763aa51238ccf45cfdfbd8d
Günther Foidl <gue@korporal.at>
2018-05-05 16:29:00 +0200

    PR feedback
    * https://github.com/gfoidl/Stochastics/pull/71#discussion_r186263509

--- tests/gfoidl.Stochastics.Native-win-x64.Tests/special_functions.cpp
+++ tests/gfoidl.Stochastics.Native-win-x64.Tests/special_functions.cpp
@@ -7,24 +7,28 @@ namespace gfoidlStochasticsNativewinx64Tests
 {
     TEST_CLASS(special_functions_Tests)
     {
+    private:
+        static double _input[];
+        static double _expectedErf[];
+
     public:
         TEST_METHOD(gaussian_error_function___OK)
         {
-            double input[]    = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5};
-            double expected[] = {0.0, 0.5204999, 0.8427008, 0.9661051, 0.9953223, 0.9995930, 0.9999779, 0.9999993};
+            double* input       = _input;
+            double* expectedErf = _expectedErf;
 
             for (int i = 0; i < 8; ++i)
             {
                 double actual = gaussian_error_function(input[i]);
 
-                Assert::AreEqual(expected[i], actual, 1e-6);
+                Assert::AreEqual(expectedErf[i], actual, 1e-6);
             }
         }
         //---------------------------------------------------------------------
         TEST_METHOD(gaussian_error_function_complementary___OK)
         {
-            double input[]       = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5};
-            double expectedErf[] = {0.0, 0.5204999, 0.8427008, 0.9661051, 0.9953223, 0.9995930, 0.9999779, 0.9999993};
+            double* input       = _input;
+            double* expectedErf = _expectedErf;
 
             for (int i = 0; i < 8; ++i)
             {
@@ -36,20 +40,20 @@ namespace gfoidlStochasticsNativewinx64Tests
         //---------------------------------------------------------------------
         TEST_METHOD(gaussian_error_function_vector___OK)
         {
-            double input[]    = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5};
-            double expected[] = {0.0, 0.5204999, 0.8427008, 0.9661051, 0.9953223, 0.9995930, 0.9999779, 0.9999993};
+            double* input       = _input;
+            double* expectedErf = _expectedErf;
             double actual[8];
 
             gaussian_error_function_vector(input, actual, 8);
 
             for (int i = 0; i < 8; ++i)
-                Assert::AreEqual(expected[i], actual[i], 1e-6);
+                Assert::AreEqual(expectedErf[i], actual[i], 1e-6);
         }
         //---------------------------------------------------------------------
         TEST_METHOD(gaussian_error_function_complementary_vector___OK)
         {
-            double input[]       = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5};
-            double expectedErf[] = {0.0, 0.5204999, 0.8427008, 0.9661051, 0.9953223, 0.9995930, 0.9999779, 0.9999993};
+            double* input       = _input;
+            double* expectedErf = _expectedErf;
             double actual[8];
 
             gaussian_error_function_complementary_vector(input, actual, 8);
@@ -58,4 +62,7 @@ namespace gfoidlStochasticsNativewinx64Tests
                 Assert::AreEqual(1.0 - expectedErf[i], actual[i], 1e-6);
         }
     };
+    //-------------------------------------------------------------------------
+    double special_functions_Tests::_input[]       = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5};
+    double special_functions_Tests::_expectedErf[] = {0.0, 0.5204999, 0.8427008, 0.9661051, 0.9953223, 0.9995930, 0.9999779, 0.9999993};
 }
