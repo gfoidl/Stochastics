647b50c6def9d718e3cacd14a2619134ad29a1ec
Günther Foidl <gue@korporal.at>
2019-04-09 10:40:11 +0200

    [WIP] XorShifter
    https://de.wikipedia.org/wiki/Xorshift

---
+++ source/gfoidl.Stochastics/RandomNumbers/XorShifter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gfoidl.Stochastics.RandomNumbers
+{
+    public  class XorShifter
+    {
+        private uint _x32 = 314159265;
+        private ulong _x64 = 88172645463325252;
+        //---------------------------------------------------------------------
+        public uint XorShift32()
+        {
+            uint x32 = _x32;
+
+            x32 ^= x32 << 13;
+            x32 ^= x32 >> 17;
+            x32 ^= x32 << 5;
+
+            return _x32 = x32;
+        }
+        //---------------------------------------------------------------------
+        public ulong XorShift64()
+        {
+            ulong x64 = _x64;
+
+            x64 ^= x64 << 13;
+            x64 ^= x64 >> 7;
+            x64 ^= x64 << 17;
+
+            return _x64 = x64;
+        }
+    }
+}
